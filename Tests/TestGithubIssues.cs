9600e162ebe3e4e5cb94bf50f8a23698c90bdadb
Guiorgy <Guiorgy123@gmail.com>
2022-04-23 18:33:09 +0400

    better test grouping
    and fixed some linter warnings in tests

--- Tests/TestGithubIssues.cs
+++ Tests/TestGithubIssues.cs
@@ -1,14 +1,15 @@
 ﻿using GitignoreParserNet;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Text;
 
-namespace Tests
+namespace Tests.TestGithubIssues
 {
-    [TestClass]
-    public class TestGithubIssues
+    [SuppressMessage("Style", "IDE1006:Naming Styles")]
+    internal static class TestHelper
     {
-        private static void gitignoreAccepts(GitignoreParser gitignore, string str, bool expected)
+        public static void gitignoreAccepts(GitignoreParser gitignore, string str, bool expected)
         {
             Assert.AreEqual(expected,
 #if DEBUG
@@ -19,7 +20,7 @@ namespace Tests
                     , $"Expected '{str}' to be {(expected ? "accepted" : "rejected")}!");
         }
 
-        private static void gitignoreDenies(GitignoreParser gitignore, string str, bool expected)
+        public static void gitignoreDenies(GitignoreParser gitignore, string str, bool expected)
         {
             Assert.AreEqual(expected,
 #if DEBUG
@@ -29,122 +30,122 @@ namespace Tests
 #endif
                     , $"Expected '{str}' to be {(expected ? "denied" : "accepted")}!");
         }
+    }
 
-        [TestClass]
-        public class TestTestCaseA
+    [TestClass]
+    public class TestCaseA
+    {
+        [TestMethod("should only accept a/2/a")]
+        public void OnlyAcceptA2A()
         {
-            [TestMethod("should only accept a/2/a")]
-            public void OnlyAcceptA2A()
-            {
-                var gitignore = new GitignoreParser(File.ReadAllText(@"..\..\..\a\.gitignore-fixture", Encoding.UTF8));
-                gitignoreAccepts(gitignore, "a/2/a", true);
-                gitignoreAccepts(gitignore, "a/3/a", false);
-            }
+            var gitignore = new GitignoreParser(File.ReadAllText(@"..\..\..\a\.gitignore-fixture", Encoding.UTF8));
+            TestHelper.gitignoreAccepts(gitignore, "a/2/a", true);
+            TestHelper.gitignoreAccepts(gitignore, "a/3/a", false);
+        }
+    }
+
+    [TestClass]
+    public class TestIssueN11
+    {
+        [TestMethod("should not fail test A")]
+        public void TestA()
+        {
+            var gitignore = new GitignoreParser("foo.txt");
+            TestHelper.gitignoreDenies(gitignore, "foo.txt", true);
+            TestHelper.gitignoreAccepts(gitignore, "foo.txt", false);
         }
 
-        [TestClass]
-        public class TestIssueN11
+        [TestMethod("should not fail test B")]
+        public void TestB()
         {
-            [TestMethod("should not fail test A")]
-            public void TestA()
-            {
-                var gitignore = new GitignoreParser("foo.txt");
-                gitignoreDenies(gitignore, "foo.txt", true);
-                gitignoreAccepts(gitignore, "foo.txt", false);
-            }
+            var gitignore = new GitignoreParser("foo.txt");
+            TestHelper.gitignoreDenies(gitignore, "a/foo.txt", true);
+            TestHelper.gitignoreAccepts(gitignore, "a/foo.txt", false);
+        }
+    }
 
-            [TestMethod("should not fail test B")]
-            public void TestB()
-            {
-                var gitignore = new GitignoreParser("foo.txt");
-                gitignoreDenies(gitignore, "a/foo.txt", true);
-                gitignoreAccepts(gitignore, "a/foo.txt", false);
-            }
+    [TestClass]
+    public class TestIssueN12
+    {
+        [TestMethod("should not fail test A")]
+        public void TestA()
+        {
+            var gitignore = new GitignoreParser("/ajax/libs/bPopup/*b*");
+            TestHelper.gitignoreAccepts(gitignore, "/ajax/libs/bPopup/0.9.0", true);  //output false
         }
 
-        [TestClass]
-        public class TestIssueN12
+        [TestMethod("should not fail test B")]
+        public void TestB()
         {
-            [TestMethod("should not fail test A")]
-            public void TestA()
-            {
-                var gitignore = new GitignoreParser("/ajax/libs/bPopup/*b*");
-                gitignoreAccepts(gitignore, "/ajax/libs/bPopup/0.9.0", true);  //output false
-            }
-
-            [TestMethod("should not fail test B")]
-            public void TestB()
-            {
-                var gitignore = new GitignoreParser("/ajax/libs/jquery-form-validator/2.2");
-                gitignoreAccepts(gitignore, "/ajax/libs/jquery-form-validator/2.2.43", true); //output false
-            }
-
-            [TestMethod("should not fail test C")]
-            public void TestC()
-            {
-                var gitignore = new GitignoreParser("/ajax/libs/punycode/2.0");
-                gitignoreAccepts(gitignore, "/ajax/libs/punycode/2.0.0", true); //output false
-            }
-
-            [TestMethod("should not fail test D")]
-            public void TestD()
-            {
-                var gitignore = new GitignoreParser("/ajax/libs/typescript/*dev*");
-                gitignoreAccepts(gitignore, "/ajax/libs/typescript/2.0.6-insiders.20161014", true); //output false
-            }
+            var gitignore = new GitignoreParser("/ajax/libs/jquery-form-validator/2.2");
+            TestHelper.gitignoreAccepts(gitignore, "/ajax/libs/jquery-form-validator/2.2.43", true); //output false
         }
 
-        [TestClass]
-        public class TestIssueN14
+        [TestMethod("should not fail test C")]
+        public void TestC()
         {
-            [TestMethod("should not fail test A")]
-            public void TestA()
-            {
-                var gitignore = new GitignoreParser("node-modules");
-                gitignoreDenies(gitignore, "packages/my-package/node-modules", true);
-                gitignoreAccepts(gitignore, "packages/my-package/node-modules", false);
-
-                gitignoreDenies(gitignore, "packages/my-package/node-modules/a", true);
-                gitignoreAccepts(gitignore, "packages/my-package/node-modules/a", false);
-
-                gitignoreDenies(gitignore, "node-modules/a", true);
-                gitignoreAccepts(gitignore, "node-modules/a", false);
-            }
+            var gitignore = new GitignoreParser("/ajax/libs/punycode/2.0");
+            TestHelper.gitignoreAccepts(gitignore, "/ajax/libs/punycode/2.0.0", true); //output false
         }
+
+        [TestMethod("should not fail test D")]
+        public void TestD()
+        {
+            var gitignore = new GitignoreParser("/ajax/libs/typescript/*dev*");
+            TestHelper.gitignoreAccepts(gitignore, "/ajax/libs/typescript/2.0.6-insiders.20161014", true); //output false
+        }
+    }
+
+    [TestClass]
+    public class TestIssueN14
+    {
+        [TestMethod("should not fail test A")]
+        public void TestA()
+        {
+            var gitignore = new GitignoreParser("node-modules");
+            TestHelper.gitignoreDenies(gitignore, "packages/my-package/node-modules", true);
+            TestHelper.gitignoreAccepts(gitignore, "packages/my-package/node-modules", false);
+
+            TestHelper.gitignoreDenies(gitignore, "packages/my-package/node-modules/a", true);
+            TestHelper.gitignoreAccepts(gitignore, "packages/my-package/node-modules/a", false);
+
+            TestHelper.gitignoreDenies(gitignore, "node-modules/a", true);
+            TestHelper.gitignoreAccepts(gitignore, "node-modules/a", false);
+        }
+    }
 
 #if DEBUG
-        [TestClass]
-        // when using the `expected` argument, accepts/denies should fire the `Diagnose()` callback
-        public class TestDiagnose
+    [TestClass]
+    // when using the `expected` argument, accepts/denies should fire the `Diagnose()` callback
+    public class TestDiagnose
+    {
+        [TestMethod("Accepts() should fail and call Diagnose()")]
+        public void TestA()
         {
-            [TestMethod("Accepts() should fail and call Diagnose()")]
-            public void TestA()
-            {
-                var gitignore = new GitignoreParser("node_modules");
-                int count = 0;
-                gitignore.OnFail += (sender, e) => count++;
+            var gitignore = new GitignoreParser("node_modules");
+            int count = 0;
+            gitignore.OnFail += (sender, e) => count++;
 
-                Assert.AreEqual(false, gitignore.Accepts("node_modules", true  /* INTENTIONALLY WRONG */), "Accepts() call is expected to return FALSE!");
-                Assert.AreEqual(1, count, "gitignore.Diagnose() should have been invoked once from inside the Accepts() call!");
-                // the next one is matching the expectation, hence no Diagnose() call:
-                Assert.AreEqual(false, gitignore.Accepts("node_modules", false), "Accepts() call is expected to return FALSE!");
-                Assert.AreEqual(1, count, "gitignore.Diagnose() should have been invoked once from inside the Accepts() call!");
+            Assert.AreEqual(false, gitignore.Accepts("node_modules", true  /* INTENTIONALLY WRONG */), "Accepts() call is expected to return FALSE!");
+            Assert.AreEqual(1, count, "gitignore.Diagnose() should have been invoked once from inside the Accepts() call!");
+            // the next one is matching the expectation, hence no Diagnose() call:
+            Assert.AreEqual(false, gitignore.Accepts("node_modules", false), "Accepts() call is expected to return FALSE!");
+            Assert.AreEqual(1, count, "gitignore.Diagnose() should have been invoked once from inside the Accepts() call!");
 
-                Assert.AreEqual(true, gitignore.Denies("node_modules", false  /* INTENTIONALLY WRONG */), "Denies() call is expected to return TRUE!");
-                Assert.AreEqual(2, count, "gitignore.Diagnose() should have been invoked once from inside the Denies() call!");
-                // the next one is matching the expectation, hence no Diagnose() call:
-                Assert.AreEqual(true, gitignore.Denies("node_modules", true), "Denies() call is expected to return TRUE!");
-                Assert.AreEqual(2, count, "gitignore.Diagnose() should have been invoked once from inside the Denies() call!");
+            Assert.AreEqual(true, gitignore.Denies("node_modules", false  /* INTENTIONALLY WRONG */), "Denies() call is expected to return TRUE!");
+            Assert.AreEqual(2, count, "gitignore.Diagnose() should have been invoked once from inside the Denies() call!");
+            // the next one is matching the expectation, hence no Diagnose() call:
+            Assert.AreEqual(true, gitignore.Denies("node_modules", true), "Denies() call is expected to return TRUE!");
+            Assert.AreEqual(2, count, "gitignore.Diagnose() should have been invoked once from inside the Denies() call!");
 
-                Assert.AreEqual(true, gitignore.Inspects("node_modules", false  /* INTENTIONALLY WRONG */), "Inspects() call is expected to return TRUE!");
-                Assert.AreEqual(3, count, "gitignore.Diagnose() should have been invoked once from inside the Inspects() call!");
-                // the next one is matching the expectation, hence no Diagnose() call:
-                Assert.AreEqual(true, gitignore.Inspects("node_modules", true), "Inspects() call is expected to return TRUE!");
-                Assert.AreEqual(3, count, "gitignore.Diagnose() should have been invoked once from inside the Inspects() call!");
-            }
+            Assert.AreEqual(true, gitignore.Inspects("node_modules", false  /* INTENTIONALLY WRONG */), "Inspects() call is expected to return TRUE!");
+            Assert.AreEqual(3, count, "gitignore.Diagnose() should have been invoked once from inside the Inspects() call!");
+            // the next one is matching the expectation, hence no Diagnose() call:
+            Assert.AreEqual(true, gitignore.Inspects("node_modules", true), "Inspects() call is expected to return TRUE!");
+            Assert.AreEqual(3, count, "gitignore.Diagnose() should have been invoked once from inside the Inspects() call!");
         }
+    }
 #endif
 
-        /* TODO: Consider using the "fixtures/gitignore.manpage.txt" file to generate more tests. */
-    }
+    /* TODO: Consider using the "fixtures/gitignore.manpage.txt" file to generate more tests. */
 }
--- Tests/TestRegex.cs
+++ Tests/TestRegex.cs
@@ -4,85 +4,85 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.IO;
 using System.Text;
 
-namespace Tests
+namespace Tests.TestRegex
 {
     [TestClass]
     public class TestRegex
     {
         readonly string fixture;
-        readonly string noNegativeFixture;
+        //readonly string noNegativeFixture;
 
         public TestRegex()
         {
             fixture = File.ReadAllText(@"..\..\..\.gitignore-fixture", Encoding.UTF8);
-            noNegativeFixture = File.ReadAllText(@"..\..\..\.gitignore-no-negatives", Encoding.UTF8);
+            //noNegativeFixture = File.ReadAllText(@"..\..\..\.gitignore-no-negatives", Encoding.UTF8);
         }
 
         [TestMethod("should parse some content")]
         public void ParseSomeContent()
         {
-            var parsed = GitignoreParser.Parse(fixture);
+            var (positives, negatives) = GitignoreParser.Parse(fixture);
             Assert.IsTrue(
-                parsed.positives.Item1 != null
-                && parsed.positives.Item2 != null
-                && parsed.negatives.Item1 != null
-                && parsed.negatives.Item2 != null
+                positives.Item1 != null
+                && positives.Item2 != null
+                && negatives.Item1 != null
+                && negatives.Item2 != null
             );
         }
 
         [TestMethod("should accept ** wildcards")]
         public void Accept2StarWildcards()
         {
-            var parsed = GitignoreParser.Parse("a/**/b");
-            Assert.AreEqual(@"(?:^\/a(?:\/|(?:\/.+\/))b(?:$|\/))", parsed.positives.Item1.ToString());
-            Assert.IsTrue(parsed.positives.Item2.Length == 1);
-            Assert.AreEqual(@"^\/a(?:\/|(?:\/.+\/))b(?:$|\/)", parsed.positives.Item2[0].ToString());
-            Assert.AreEqual(@"$^", parsed.negatives.Item1.ToString());
-            Assert.IsTrue(parsed.negatives.Item2.Length == 0);
+            var (positives, negatives) = GitignoreParser.Parse("a/**/b");
+            Assert.AreEqual(@"(?:^\/a(?:\/|(?:\/.+\/))b(?:$|\/))", positives.Item1.ToString());
+            Assert.IsTrue(positives.Item2.Length == 1);
+            Assert.AreEqual(@"^\/a(?:\/|(?:\/.+\/))b(?:$|\/)", positives.Item2[0].ToString());
+            Assert.AreEqual("$^", negatives.Item1.ToString());
+            Assert.IsTrue(negatives.Item2.Length == 0);
         }
 
         [TestMethod("should accept * wildcards")]
         public void Accept1StarWildcards()
         {
-            var parsed = GitignoreParser.Parse("a/b*c");
-            Assert.AreEqual(@"(?:^\/a\/b[^\/]*c(?:$|\/))", parsed.positives.Item1.ToString());
-            Assert.IsTrue(parsed.positives.Item2.Length == 1);
-            Assert.AreEqual(@"^\/a\/b[^\/]*c(?:$|\/)", parsed.positives.Item2[0].ToString());
-            Assert.AreEqual(@"$^", parsed.negatives.Item1.ToString());
-            Assert.IsTrue(parsed.negatives.Item2.Length == 0);
+            var (positives, negatives) = GitignoreParser.Parse("a/b*c");
+            Assert.AreEqual(@"(?:^\/a\/b[^\/]*c(?:$|\/))", positives.Item1.ToString());
+            Assert.IsTrue(positives.Item2.Length == 1);
+            Assert.AreEqual(@"^\/a\/b[^\/]*c(?:$|\/)", positives.Item2[0].ToString());
+            Assert.AreEqual("$^", negatives.Item1.ToString());
+            Assert.IsTrue(negatives.Item2.Length == 0);
         }
 
         [TestMethod("should accept ? wildcards")]
         public void AcceptQuestionMarkWildcards()
         {
-            var parsed = GitignoreParser.Parse("a/b?");
-            Assert.AreEqual(@"(?:^\/a\/b[^\/](?:$|\/))", parsed.positives.Item1.ToString());
-            Assert.IsTrue(parsed.positives.Item2.Length == 1);
-            Assert.AreEqual(@"^\/a\/b[^\/](?:$|\/)", parsed.positives.Item2[0].ToString());
-            Assert.AreEqual(@"$^", parsed.negatives.Item1.ToString());
-            Assert.IsTrue(parsed.negatives.Item2.Length == 0);
+            var (positives, negatives) = GitignoreParser.Parse("a/b?");
+            Assert.AreEqual(@"(?:^\/a\/b[^\/](?:$|\/))", positives.Item1.ToString());
+            Assert.IsTrue(positives.Item2.Length == 1);
+            Assert.AreEqual(@"^\/a\/b[^\/](?:$|\/)", positives.Item2[0].ToString());
+            Assert.AreEqual("$^", negatives.Item1.ToString());
+            Assert.IsTrue(negatives.Item2.Length == 0);
         }
 
         [TestMethod("should correctly encode literals")]
         public void EncodeLiterals()
         {
-            var parsed = GitignoreParser.Parse(@"a/b.c[d](e){f}\slash^_$+$$$");
-            Assert.AreEqual(@"(?:^\/a\/b\.c[d]\(e\)\{f\}slash\^_\$\+\$\$\$(?:$|\/))", parsed.positives.Item1.ToString());
-            Assert.IsTrue(parsed.positives.Item2.Length == 1);
-            Assert.AreEqual(@"^\/a\/b\.c[d]\(e\)\{f\}slash\^_\$\+\$\$\$(?:$|\/)", parsed.positives.Item2[0].ToString());
-            Assert.AreEqual(@"$^", parsed.negatives.Item1.ToString());
-            Assert.IsTrue(parsed.negatives.Item2.Length == 0);
+            var (positives, negatives) = GitignoreParser.Parse(@"a/b.c[d](e){f}\slash^_$+$$$");
+            Assert.AreEqual(@"(?:^\/a\/b\.c[d]\(e\)\{f\}slash\^_\$\+\$\$\$(?:$|\/))", positives.Item1.ToString());
+            Assert.IsTrue(positives.Item2.Length == 1);
+            Assert.AreEqual(@"^\/a\/b\.c[d]\(e\)\{f\}slash\^_\$\+\$\$\$(?:$|\/)", positives.Item2[0].ToString());
+            Assert.AreEqual("$^", negatives.Item1.ToString());
+            Assert.IsTrue(negatives.Item2.Length == 0);
         }
 
         [TestMethod("should correctly parse character ranges")]
         public void ParseCharacterRanges()
         {
-            var parsed = GitignoreParser.Parse(@"a[c-z$].[1-9-][\[\]A-Z]-\[...]");
-            Assert.AreEqual(@"(?:\/a[c-z$]\.[1-9-][\[\]A-Z]\-\[\.\.\.\](?:$|\/))", parsed.positives.Item1.ToString());
-            Assert.IsTrue(parsed.positives.Item2.Length == 1);
-            Assert.AreEqual(@"\/a[c-z$]\.[1-9-][\[\]A-Z]\-\[\.\.\.\](?:$|\/)", parsed.positives.Item2[0].ToString());
-            Assert.AreEqual(@"$^", parsed.negatives.Item1.ToString());
-            Assert.IsTrue(parsed.negatives.Item2.Length == 0);
+            var (positives, negatives) = GitignoreParser.Parse(@"a[c-z$].[1-9-][\[\]A-Z]-\[...]");
+            Assert.AreEqual(@"(?:\/a[c-z$]\.[1-9-][\[\]A-Z]\-\[\.\.\.\](?:$|\/))", positives.Item1.ToString());
+            Assert.IsTrue(positives.Item2.Length == 1);
+            Assert.AreEqual(@"\/a[c-z$]\.[1-9-][\[\]A-Z]\-\[\.\.\.\](?:$|\/)", positives.Item2[0].ToString());
+            Assert.AreEqual("$^", negatives.Item1.ToString());
+            Assert.IsTrue(negatives.Item2.Length == 0);
         }
 
         [TestMethod("should treat \"/a\" as rooted, while \"b\" should match anywhere")]
@@ -94,27 +94,27 @@ namespace Tests
         // below the .gitignore level.
         public void ARootedBAnywhere()
         {
-            var parsed = GitignoreParser.Parse("/a\nb");
-            Assert.AreEqual(@"(?:^\/a(?:$|\/))|(?:\/b(?:$|\/))", parsed.positives.Item1.ToString());
-            Assert.IsTrue(parsed.positives.Item2.Length == 2);
-            Assert.AreEqual(@"^\/a(?:$|\/)", parsed.positives.Item2[0].ToString());
-            Assert.AreEqual(@"\/b(?:$|\/)", parsed.positives.Item2[1].ToString());
-            Assert.AreEqual(@"$^", parsed.negatives.Item1.ToString());
-            Assert.IsTrue(parsed.negatives.Item2.Length == 0);
+            var (positives, negatives) = GitignoreParser.Parse("/a\nb");
+            Assert.AreEqual(@"(?:^\/a(?:$|\/))|(?:\/b(?:$|\/))", positives.Item1.ToString());
+            Assert.IsTrue(positives.Item2.Length == 2);
+            Assert.AreEqual(@"^\/a(?:$|\/)", positives.Item2[0].ToString());
+            Assert.AreEqual(@"\/b(?:$|\/)", positives.Item2[1].ToString());
+            Assert.AreEqual("$^", negatives.Item1.ToString());
+            Assert.IsTrue(negatives.Item2.Length == 0);
         }
 
         [TestMethod("should correctly transpile rooted and relative path specs")]
         public void TranspileRootedAndRelative()
         {
-            var parsed = GitignoreParser.Parse("a/b\n/c/d\ne/\nf");
-            Assert.AreEqual(@"(?:^\/c\/d(?:$|\/))|(?:^\/a\/b(?:$|\/))|(?:\/e\/)|(?:\/f(?:$|\/))", parsed.positives.Item1.ToString());
-            Assert.IsTrue(parsed.positives.Item2.Length == 4);
-            Assert.AreEqual(@"^\/c\/d(?:$|\/)", parsed.positives.Item2[0].ToString());
-            Assert.AreEqual(@"^\/a\/b(?:$|\/)", parsed.positives.Item2[1].ToString());
-            Assert.AreEqual(@"\/e\/", parsed.positives.Item2[2].ToString());
-            Assert.AreEqual(@"\/f(?:$|\/)", parsed.positives.Item2[3].ToString());
-            Assert.AreEqual(@"$^", parsed.negatives.Item1.ToString());
-            Assert.IsTrue(parsed.negatives.Item2.Length == 0);
+            var (positives, negatives) = GitignoreParser.Parse("a/b\n/c/d\ne/\nf");
+            Assert.AreEqual(@"(?:^\/c\/d(?:$|\/))|(?:^\/a\/b(?:$|\/))|(?:\/e\/)|(?:\/f(?:$|\/))", positives.Item1.ToString());
+            Assert.IsTrue(positives.Item2.Length == 4);
+            Assert.AreEqual(@"^\/c\/d(?:$|\/)", positives.Item2[0].ToString());
+            Assert.AreEqual(@"^\/a\/b(?:$|\/)", positives.Item2[1].ToString());
+            Assert.AreEqual(@"\/e\/", positives.Item2[2].ToString());
+            Assert.AreEqual(@"\/f(?:$|\/)", positives.Item2[3].ToString());
+            Assert.AreEqual("$^", negatives.Item1.ToString());
+            Assert.IsTrue(negatives.Item2.Length == 0);
         }
     }
 }
