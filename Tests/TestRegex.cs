9bd4fd85add8f89fec32a198eed8a28d9f7f9987
Guiorgy <Guiorgy123@gmail.com>
2023-11-27 17:10:51 +0400

    migrated Tests from .NET 6 to .NET 8

--- Tests/TestRegex.cs
+++ Tests/TestRegex.cs
@@ -23,10 +23,10 @@ namespace Tests.TestRegex
         {
             var (positives, negatives) = GitignoreParser.Parse(fixture);
             Assert.IsTrue(
-                positives.Item1 != null
-                && positives.Item2 != null
-                && negatives.Item1 != null
-                && negatives.Item2 != null
+                positives.Merged != null
+                && positives.Individual != null
+                && negatives.Merged != null
+                && negatives.Individual != null
             );
         }
 
@@ -34,55 +34,55 @@ namespace Tests.TestRegex
         public void Accept2StarWildcards()
         {
             var (positives, negatives) = GitignoreParser.Parse("a/**/b");
-            Assert.AreEqual(@"(?:^\/a(?:\/|(?:\/.+\/))b(?:$|\/))", positives.Item1.ToString());
-            Assert.IsTrue(positives.Item2.Length == 1);
-            Assert.AreEqual(@"^\/a(?:\/|(?:\/.+\/))b(?:$|\/)", positives.Item2[0].ToString());
-            Assert.AreEqual("$^", negatives.Item1.ToString());
-            Assert.IsTrue(negatives.Item2.Length == 0);
+            Assert.AreEqual(@"(?:^\/a(?:\/|(?:\/.+\/))b(?:$|\/))", positives.Merged.ToString());
+            Assert.IsTrue(positives.Individual.Length == 1);
+            Assert.AreEqual(@"^\/a(?:\/|(?:\/.+\/))b(?:$|\/)", positives.Individual[0].ToString());
+            Assert.AreEqual("$^", negatives.Merged.ToString());
+            Assert.IsTrue(negatives.Individual.Length == 0);
         }
 
         [TestMethod("should accept * wildcards")]
         public void Accept1StarWildcards()
         {
             var (positives, negatives) = GitignoreParser.Parse("a/b*c");
-            Assert.AreEqual(@"(?:^\/a\/b[^\/]*c(?:$|\/))", positives.Item1.ToString());
-            Assert.IsTrue(positives.Item2.Length == 1);
-            Assert.AreEqual(@"^\/a\/b[^\/]*c(?:$|\/)", positives.Item2[0].ToString());
-            Assert.AreEqual("$^", negatives.Item1.ToString());
-            Assert.IsTrue(negatives.Item2.Length == 0);
+            Assert.AreEqual(@"(?:^\/a\/b[^\/]*c(?:$|\/))", positives.Merged.ToString());
+            Assert.IsTrue(positives.Individual.Length == 1);
+            Assert.AreEqual(@"^\/a\/b[^\/]*c(?:$|\/)", positives.Individual[0].ToString());
+            Assert.AreEqual("$^", negatives.Merged.ToString());
+            Assert.IsTrue(negatives.Individual.Length == 0);
         }
 
         [TestMethod("should accept ? wildcards")]
         public void AcceptQuestionMarkWildcards()
         {
             var (positives, negatives) = GitignoreParser.Parse("a/b?");
-            Assert.AreEqual(@"(?:^\/a\/b[^\/](?:$|\/))", positives.Item1.ToString());
-            Assert.IsTrue(positives.Item2.Length == 1);
-            Assert.AreEqual(@"^\/a\/b[^\/](?:$|\/)", positives.Item2[0].ToString());
-            Assert.AreEqual("$^", negatives.Item1.ToString());
-            Assert.IsTrue(negatives.Item2.Length == 0);
+            Assert.AreEqual(@"(?:^\/a\/b[^\/](?:$|\/))", positives.Merged.ToString());
+            Assert.IsTrue(positives.Individual.Length == 1);
+            Assert.AreEqual(@"^\/a\/b[^\/](?:$|\/)", positives.Individual[0].ToString());
+            Assert.AreEqual("$^", negatives.Merged.ToString());
+            Assert.IsTrue(negatives.Individual.Length == 0);
         }
 
         [TestMethod("should correctly encode literals")]
         public void EncodeLiterals()
         {
             var (positives, negatives) = GitignoreParser.Parse(@"a/b.c[d](e){f}\slash^_$+$$$");
-            Assert.AreEqual(@"(?:^\/a\/b\.c[d]\(e\)\{f\}slash\^_\$\+\$\$\$(?:$|\/))", positives.Item1.ToString());
-            Assert.IsTrue(positives.Item2.Length == 1);
-            Assert.AreEqual(@"^\/a\/b\.c[d]\(e\)\{f\}slash\^_\$\+\$\$\$(?:$|\/)", positives.Item2[0].ToString());
-            Assert.AreEqual("$^", negatives.Item1.ToString());
-            Assert.IsTrue(negatives.Item2.Length == 0);
+            Assert.AreEqual(@"(?:^\/a\/b\.c[d]\(e\)\{f\}slash\^_\$\+\$\$\$(?:$|\/))", positives.Merged.ToString());
+            Assert.IsTrue(positives.Individual.Length == 1);
+            Assert.AreEqual(@"^\/a\/b\.c[d]\(e\)\{f\}slash\^_\$\+\$\$\$(?:$|\/)", positives.Individual[0].ToString());
+            Assert.AreEqual("$^", negatives.Merged.ToString());
+            Assert.IsTrue(negatives.Individual.Length == 0);
         }
 
         [TestMethod("should correctly parse character ranges")]
         public void ParseCharacterRanges()
         {
             var (positives, negatives) = GitignoreParser.Parse(@"a[c-z$].[1-9-][\[\]A-Z]-\[...]");
-            Assert.AreEqual(@"(?:\/a[c-z$]\.[1-9-][\[\]A-Z]\-\[\.\.\.\](?:$|\/))", positives.Item1.ToString());
-            Assert.IsTrue(positives.Item2.Length == 1);
-            Assert.AreEqual(@"\/a[c-z$]\.[1-9-][\[\]A-Z]\-\[\.\.\.\](?:$|\/)", positives.Item2[0].ToString());
-            Assert.AreEqual("$^", negatives.Item1.ToString());
-            Assert.IsTrue(negatives.Item2.Length == 0);
+            Assert.AreEqual(@"(?:\/a[c-z$]\.[1-9-][\[\]A-Z]\-\[\.\.\.\](?:$|\/))", positives.Merged.ToString());
+            Assert.IsTrue(positives.Individual.Length == 1);
+            Assert.AreEqual(@"\/a[c-z$]\.[1-9-][\[\]A-Z]\-\[\.\.\.\](?:$|\/)", positives.Individual[0].ToString());
+            Assert.AreEqual("$^", negatives.Merged.ToString());
+            Assert.IsTrue(negatives.Individual.Length == 0);
         }
 
         [TestMethod("should treat \"/a\" as rooted, while \"b\" should match anywhere")]
@@ -95,26 +95,26 @@ namespace Tests.TestRegex
         public void ARootedBAnywhere()
         {
             var (positives, negatives) = GitignoreParser.Parse("/a\nb");
-            Assert.AreEqual(@"(?:^\/a(?:$|\/))|(?:\/b(?:$|\/))", positives.Item1.ToString());
-            Assert.IsTrue(positives.Item2.Length == 2);
-            Assert.AreEqual(@"^\/a(?:$|\/)", positives.Item2[0].ToString());
-            Assert.AreEqual(@"\/b(?:$|\/)", positives.Item2[1].ToString());
-            Assert.AreEqual("$^", negatives.Item1.ToString());
-            Assert.IsTrue(negatives.Item2.Length == 0);
+            Assert.AreEqual(@"(?:^\/a(?:$|\/))|(?:\/b(?:$|\/))", positives.Merged.ToString());
+            Assert.IsTrue(positives.Individual.Length == 2);
+            Assert.AreEqual(@"^\/a(?:$|\/)", positives.Individual[0].ToString());
+            Assert.AreEqual(@"\/b(?:$|\/)", positives.Individual[1].ToString());
+            Assert.AreEqual("$^", negatives.Merged.ToString());
+            Assert.IsTrue(negatives.Individual.Length == 0);
         }
 
         [TestMethod("should correctly transpile rooted and relative path specs")]
         public void TranspileRootedAndRelative()
         {
             var (positives, negatives) = GitignoreParser.Parse("a/b\n/c/d\ne/\nf");
-            Assert.AreEqual(@"(?:^\/c\/d(?:$|\/))|(?:^\/a\/b(?:$|\/))|(?:\/e\/)|(?:\/f(?:$|\/))", positives.Item1.ToString());
-            Assert.IsTrue(positives.Item2.Length == 4);
-            Assert.AreEqual(@"^\/c\/d(?:$|\/)", positives.Item2[0].ToString());
-            Assert.AreEqual(@"^\/a\/b(?:$|\/)", positives.Item2[1].ToString());
-            Assert.AreEqual(@"\/e\/", positives.Item2[2].ToString());
-            Assert.AreEqual(@"\/f(?:$|\/)", positives.Item2[3].ToString());
-            Assert.AreEqual("$^", negatives.Item1.ToString());
-            Assert.IsTrue(negatives.Item2.Length == 0);
+            Assert.AreEqual(@"(?:^\/c\/d(?:$|\/))|(?:^\/a\/b(?:$|\/))|(?:\/e\/)|(?:\/f(?:$|\/))", positives.Merged.ToString());
+            Assert.IsTrue(positives.Individual.Length == 4);
+            Assert.AreEqual(@"^\/c\/d(?:$|\/)", positives.Individual[0].ToString());
+            Assert.AreEqual(@"^\/a\/b(?:$|\/)", positives.Individual[1].ToString());
+            Assert.AreEqual(@"\/e\/", positives.Individual[2].ToString());
+            Assert.AreEqual(@"\/f(?:$|\/)", positives.Individual[3].ToString());
+            Assert.AreEqual("$^", negatives.Merged.ToString());
+            Assert.IsTrue(negatives.Individual.Length == 0);
         }
     }
 }
